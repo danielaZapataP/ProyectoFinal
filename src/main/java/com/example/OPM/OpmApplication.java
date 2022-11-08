3e48a3cd2a58aba28d105aea61506938d025d1fc
Daniela Zapata Pino <dazapi9@gmail.com>
2022-11-08 17:07:08 -0500

    Inicio proyecto

---
+++ src/main/java/com/example/OPM/OpmApplication.java
@@ -0,0 +1,13 @@
+package com.example.OPM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpmApplication.class, args);
+	}
+
+}
